92dc9d7971b2e457729a3fd2e92bbd1ebb13dbb4
zshan <zshann992@gmail.com>
2024-04-12 23:05:55 +0500

    Server: 1. print 'Starting' in __main__

--- server/app.py
+++ server/app.py
@@ -6,4 +6,5 @@ App.enable_cors()
 from api import _ # noqa
 
 if __name__ == '__main__':
+    print('Starting app - AI-Disease Predictor - __main__')
     App.run_app(debug=True, host='0.0.0.0', port=80)
