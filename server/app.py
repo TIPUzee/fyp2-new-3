9c44487d42e620227c355c74b17cbb350d302ec8
zshan <zshann992@gmail.com>
2024-04-12 23:45:34 +0500

    .

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from utils import App
+from utils import App, Env
 
 App.init_app('AI-Disease Predictor')
 App.enable_cors()
@@ -7,4 +7,5 @@ from api import _ # noqa
 
 if __name__ == '__main__':
     print('Starting app - AI-Disease Predictor - __main__')
+    print('Current Mode:', 'Production' if Env.get('PROD_MODE') else 'Development')
     App.run_app(debug=True, host='0.0.0.0', port=80)
