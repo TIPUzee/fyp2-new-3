3f46737cc884d9947f3f8f51d53fbb19189ef243
zshan <zshann992@gmail.com>
2024-04-13 01:19:10 +0500

    Server port 5000

--- server/app.py
+++ server/app.py
@@ -8,4 +8,4 @@ from api import _ # noqa
 if __name__ == '__main__':
     print('Starting app - AI-Disease Predictor - __main__')
     print('Current Mode:', 'Production' if Env.get('PROD_MODE') else 'Development')
-    App.run_app(debug=True, host='0.0.0.0', port=80)
+    App.run_app(debug=True, host='0.0.0.0', port=5000)
